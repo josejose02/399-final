87fa3133c84498240fc96349de16fdb49be84401
Jose David <josedvzsg@gmail.com>
2021-12-10 12:26:06 -0500

    ok

--- congress/script.js
+++ congress/script.js
@@ -1,11 +1,2 @@
-var data;
-fetch("data.json")
-.then(response => response.json())
-.then(data => {
-    
-});
 
 
-document.querySelector("#p-01").addEventListener("click", ()=>{
-    console.log("Test");
-});
